6e2667ea601396cb162ea70af5b0bed3c7b89322
MrFish486 <laszlodeanpav@gmail.com>
2026-07-15 15:08:30 -0700

    Done!

--- elf-info.c
+++ elf-info.c
@@ -34,10 +34,10 @@ int main (int argc, char **argv){
 		int address_space = 0;
 		// First step is to read the magic number
 		if (buffer[0x00] == 0x7f && buffer[0x01] == 0x45 && buffer[0x02] == 0x4c && buffer[0x03] == 0x46) {
-			printf("File seems to be valid ELF file\n");
-			printf("-------------------------------\n");
+			printf("File seems to be valid ELF file, reading...\n");
+			printf("----------------------------------\n");
 		}
-		printf("Magic number                  : %02hx %02hx %02hx %02hx | %c%c%c%c\n", buffer[0x00], buffer[0x01], buffer[0x02], buffer[0x03], buffer[0x00], buffer[0x01], buffer[0x02], buffer[0x03]);
+		printf("Magic number                     : %02hx %02hx %02hx %02hx\n", buffer[0x00], buffer[0x01], buffer[0x02], buffer[0x03]);
 		// Now address space check
 		if (buffer[0x04] == 0x01) {
 			address_space = 32;
@@ -47,9 +47,9 @@ int main (int argc, char **argv){
 			fprintf(stderr, "Invalid address space identifier at 0x04\n");
 			return 3;
 		}
-		printf("Address space                 : %d-bit\n", address_space);
+		printf("Address space                    : %d-bit\n", address_space);
 		// Now for endianness
-		printf("Endianness                    : ");
+		printf("Endianness                       : ");
 		if (buffer[0x05] == 0x01) {
 			printf("little endian\n");
 		} else if (buffer[0x06] == 0x02) {
@@ -60,9 +60,9 @@ int main (int argc, char **argv){
 		}
 		endianness = buffer[0x06];
 		// ELF version
-		printf("ELF version                   : %02hx\n", buffer[0x06]);
+		printf("ELF version                      : %02hx\n", buffer[0x06]);
 		// ABI
-		printf("OS ABI                        : %02hx (", buffer[0x07]);
+		printf("OS ABI                           : %02hx (", buffer[0x07]);
 		switch (buffer[0x07]) {
 			case 0x00:
 				printf("System V");
@@ -123,10 +123,10 @@ int main (int argc, char **argv){
 		}
 		printf(")\n");
 		// ABI Version
-		printf("ABI Version                   : %02hx\n", buffer[0x08]);
+		printf("ABI Version                      : %02hx\n", buffer[0x08]);
 		// Padding from 0x09 -> 0x10
 		// Type
-		printf("Type                          : ");
+		printf("Type                             : ");
 		switch (get_field(endianness, 2, buffer[0x10], buffer[0x11])) {
 			case 0x00:
 				printf("ET_NONE (Unknown)");
@@ -160,7 +160,7 @@ int main (int argc, char **argv){
 		}
 		printf("\n");
 		// Target architecture... :(
-		printf("Instruction Set Architecture  : %02hx%02hx (", buffer[0x12], buffer[0x13]);
+		printf("Instruction Set Architecture     : %02hx %02hx (", buffer[0x12], buffer[0x13]);
 		switch (get_field(endianness, 2, buffer[0x12], buffer[0x13])) {
 			case 0x01:
 				printf("AT&T WE 32100");
@@ -365,31 +365,144 @@ int main (int argc, char **argv){
 		}
 		printf(")\n");
 		// Version Detail
-		printf("Version detail                : %02hx%02hx%02hx%02hx\n", buffer[0x14], buffer[0x15], buffer[0x16], buffer[0x17]);
-		printf("-------------------------------\n");
+		printf("Version detail                   : %02hx %02hx %02hx %02hx\n", buffer[0x14], buffer[0x15], buffer[0x16], buffer[0x17]);
+		printf("---------------------------------+\n");
 		// Program entry
-		printf("Program entry point           : ");
+		printf("Program entry point              : ");
 		if (address_space == 32) {
-			printf("%02hx%02hx%02hx%02hx", buffer[0x18], buffer[0x19], buffer[0x1A], buffer[0x1B]);
+			printf("%02hx %02hx %02hx %02hx", buffer[0x18], buffer[0x19], buffer[0x1A], buffer[0x1B]);
 		} else if (address_space == 64) {
-			printf("%02hx%02hx%02hx%02hx %02hx%02hx%02hx%02hx", buffer[0x18], buffer[0x19], buffer[0x1A], buffer[0x1B], buffer[0x1C], buffer[0x1D], buffer[0x1E], buffer[0x1F]);
+			printf("%02hx %02hx %02hx %02hx  %02hx %02hx %02hx %02hx", buffer[0x18], buffer[0x19], buffer[0x1A], buffer[0x1B], buffer[0x1C], buffer[0x1D], buffer[0x1E], buffer[0x1F]);
 		}
 		printf("\n");
 		// Program header table
-		printf("Program header table          : ");
+		printf("Program header table             : ");
 		if (address_space == 32) {
-			printf("%02hx%02hx%02hx%02hx", buffer[0x1C], buffer[0x1D], buffer[0x1E], buffer[0x1F]);
+			printf("%02hx %02hx %02hx %02hx", buffer[0x1C], buffer[0x1D], buffer[0x1E], buffer[0x1F]);
 		} else if (address_space == 64) {
-			printf("%02hx%02hx%02hx%02hx %02hx%02hx%02hx%02hx", buffer[0x20], buffer[0x21], buffer[0x22], buffer[0x23], buffer[0x24], buffer[0x25], buffer[0x26], buffer[0x27]);
+			printf("%02hx %02hx %02hx %02hx  %02hx %02hx %02hx %02hx", buffer[0x20], buffer[0x21], buffer[0x22], buffer[0x23], buffer[0x24], buffer[0x25], buffer[0x26], buffer[0x27]);
 		}
 		printf("\n");
 		// Section header table
-		printf("Section header table          : ");
+		printf("Section header table             : ");
 		if (address_space == 32) {
-			printf("%02hx%02hx%02hx%02hx", buffer[0x20], buffer[0x21], buffer[0x22], buffer[0x23]);
+			printf("%02hx %02hx %02hx %02hx", buffer[0x20], buffer[0x21], buffer[0x22], buffer[0x23]);
 		} else if (address_space == 64) {
-			printf("%02hx%02hx%02hx%02hx %02hx%02hx%02hx%02hx", buffer[0x28], buffer[0x29], buffer[0x2A], buffer[0x2B], buffer[0x2C], buffer[0x2D], buffer[0x2E], buffer[0x2F]);
-		}	
+			printf("%02hx %02hx %02hx %02hx  %02hx %02hx %02hx %02hx", buffer[0x28], buffer[0x29], buffer[0x2A], buffer[0x2B], buffer[0x2C], buffer[0x2D], buffer[0x2E], buffer[0x2F]);
+		}
+		printf("\n");
+		// Flags
+		printf("Flags (ISA dependent)            : ");
+		if (address_space == 32) {
+			printf("%02hx %02hx %02hx %02hx", buffer[0x24], buffer[0x25], buffer[0x26], buffer[0x27]);
+		} else if (address_space == 64) {
+			printf("%02hx %02hx %02hx %02hx", buffer[0x30], buffer[0x31], buffer[0x32], buffer[0x33]);
+		}
+		printf("\n");
+		// Header size
+		printf("ELF Header size                  : ");
+		if (address_space == 32) {
+			printf("%02hx %02hx", buffer[0x28], buffer[0x29]);
+			printf(" (%d)", get_field(endianness, 2, buffer[0x28], buffer[0x29]));
+		} else if (address_space == 64) {
+			printf("%02hx %02hx", buffer[0x34], buffer[0x35]);
+			printf(" (%d)", get_field(endianness, 2, buffer[0x34], buffer[0x35]));
+		}
+		printf("\n");
+		// Program header table size
+		printf("Program header table size        : ");
+		if (address_space == 32) {
+			printf("%02hx %02hx", buffer[0x2A], buffer[0x2B]);
+			printf(" (%d: ", get_field(endianness, 2, buffer[0x2A], buffer[0x2B]));
+			switch (get_field(endianness, 2, buffer[0x2A], buffer[0x2B])) {
+				case 0x20:
+					printf("32-bit, matches");
+					break;
+				case 0x38:
+					printf("64-bit, unmatching");
+					break;
+				default:
+					printf("non-standard size");
+			}
+			printf(")");
+		} else if (address_space == 64) {
+			printf("%02hx %02hx", buffer[0x36], buffer[0x37]);
+			printf(" (%d: ", get_field(endianness, 2, buffer[0x36], buffer[0x37]));
+			switch (get_field(endianness, 2, buffer[0x36], buffer[0x37])) {
+				case 0x20:
+					printf("32-bit, unmatching");
+					break;
+				case 0x38:
+					printf("64-bit, matches");
+					break;
+				default:
+					printf("non-standard size");
+			}
+			printf(")");
+		}
+		printf("\n");
+		// Program header table entry count
+		printf("Program header table entry count : ");
+		if (address_space == 32) {
+			printf("%02hx %02hx", buffer[0x2C], buffer[0x2D]);
+			printf(" (%d)", get_field(endianness, 2, buffer[0x2C], buffer[0x2D]));
+		} else if (address_space == 64) {
+			printf("%02hx %02hx", buffer[0x38], buffer[0x39]);
+			printf(" (%d)", get_field(endianness, 2, buffer[0x38], buffer[0x39]));
+		}
+		printf("\n");
+		// Section header entry size
+		printf("Section header table entry size  : ");
+		if (address_space == 32) {
+			printf("%02hx %02hx", buffer[0x2E], buffer[0x2F]);
+			printf(" (%d: ", get_field(endianness, 2, buffer[0x2E], buffer[0x2F]));
+			switch (get_field(endianness, 2, buffer[0x2E], buffer[0x2F])) {
+				case 0x28:
+					printf("32-bit, matches");
+					break;
+				case 0x40:
+					printf("64-bit, unmatching");
+					break;
+				default:
+					printf("non-standard size");
+			}
+			printf(")");
+		} else if (address_space == 64) {
+			printf("%02hx %02hx", buffer[0x3A], buffer[0x3B]);
+			printf(" (%d: ", get_field(endianness, 2, buffer[0x3A], buffer[0x3B]));
+			switch (get_field(endianness, 2, buffer[0x3A], buffer[0x3B])) {
+				case 0x28:
+					printf("32-bit, unmatching");
+					break;
+				case 0x40:
+					printf("64-bit, matches");
+					break;
+				default:
+					printf("non-standard size");
+			}
+			printf(")");
+		}
+		printf("\n");
+		// Section header table entry count
+		printf("Section header table entry count : ");
+		if (address_space == 32) {
+			printf("%02hx %02hx", buffer[0x30], buffer[0x31]);
+			printf(" (%d)", get_field(endianness, 2, buffer[0x30], buffer[0x31]));
+		} else if (address_space == 64) {
+			printf("%02hx %02hx", buffer[0x3C], buffer[0x3D]);
+			printf(" (%d)", get_field(endianness, 2, buffer[0x3C], buffer[0x3D]));
+		}
+		printf("\n");
+		// Section header table name index
+		printf("Section header table name index  : ");
+		if (address_space == 32) {
+			printf("%02hx %02hx", buffer[0x32], buffer[0x33]);
+			printf(" (%d)", get_field(endianness, 2, buffer[0x32], buffer[0x33]));
+		} else if (address_space == 64) {
+			printf("%02hx %02hx", buffer[0x3E], buffer[0x3F]);
+			printf(" (%d)", get_field(endianness, 2, buffer[0x3E], buffer[0x3F]));
+		}
+		printf("\n");
 	}
 }
 
