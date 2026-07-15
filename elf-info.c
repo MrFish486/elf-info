4e7f5528b36b7bf9c1a151c3d0f2ac9a93bff42c
MrFish486 <laszlodeanpav@gmail.com>
2026-07-15 11:08:32 -0700

    *argv -> **argv

--- elf-info.c
+++ elf-info.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 
-int main (int argc, char *argv){
+int main (int argc, char **argv){
 	if (argc == 1) {
-		fprintf(stderr, "No file specified.");
+		fprintf(stderr, "No file specified.\n");
 		return 1;
 	} else if (argc > 2) {
-		fprintf(stderr, "Too many files!");
+		fprintf(stderr, "Too many files!\n");
 		return 2;
+	} else {
+		unsigned char buffer[64];
+		FILE* handle = fopen(argv[0], "rb");
+		fread(buffer, sizeof(buffer), 1, handle);
 	}
 }
 
